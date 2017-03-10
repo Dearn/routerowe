5e45cb1aadbff90e4d1b42dddc1ea97d7fe6980f
Piotr Filek <dearn@dearn.pl>
2017-03-10 22:54:18 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+import requests
+
+r = requests.get("http://192.168.2.1/log/in?un=admin&pw=admin&rd=%2Fuir%2Fstatus.htm&rd2=%2Fuir%2Fwanst.htm&Nrd=1")
+
+data = r.text
+soup = BeautifulSoup(data, "html.parser")
+a = ""
+for td in soup.find_all('td', {'id':'ip3g'}):
+    a = td.string
+
+print(a)
