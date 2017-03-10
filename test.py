028b681a7c26f3efc7a0569ba789fbace8cfd4f8
Piotr Filek <dearn@dearn.pl>
2017-03-10 23:05:11 +0000

    a

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-from bs4 import BeautifulSoup
+#!/usr/bin/env python
 
+from bs4 import BeautifulSoup
 import requests
 
 r = requests.get("http://192.168.2.1/log/in?un=admin&pw=admin&rd=%2Fuir%2Fstatus.htm&rd2=%2Fuir%2Fwanst.htm&Nrd=1")
@@ -9,5 +10,6 @@ soup = BeautifulSoup(data, "html.parser")
 a = ""
 for td in soup.find_all('td', {'id':'ip3g'}):
     a = td.string
-
-print(a)
+    print (a)
+with open('adres.txt', 'w') as out:
+    out.write(a)
